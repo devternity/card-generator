649db77082a4bcf832145281b253047abc836d04
Andrey Adamovich <andrey@aestasit.com>
2017-09-08 22:53:54 +0300

    playing with fonts

--- src/main/groovy/lv/latcraft/utils/SvgMethods.groovy
+++ src/main/groovy/lv/latcraft/utils/SvgMethods.groovy
@@ -32,7 +32,10 @@ class SvgMethods {
   static File renderPNG(File svgFile) {
     def t = new PNGTranscoder()
     t.addTranscodingHint(KEY_BACKGROUND_COLOR, Color.WHITE)
-    // configureFonts(t)
+    t.addTranscodingHint(KEY_PIXEL_UNIT_TO_MILLIMETER, new Float((float) (25.4 / DEFAULT_DPI)))
+    t.addTranscodingHint(KEY_XML_PARSER_VALIDATING, FALSE)
+    t.addTranscodingHint(KEY_STROKE_TEXT, FALSE)
+    t.addTranscodingHint(KEY_AUTO_FONTS, false)
     String svgURI = svgFile.toURI().toString()
     File pngFile = temporaryFile('temporary', '.png')
     try {
@@ -51,57 +54,6 @@ class SvgMethods {
     }
     pngFile
 
-  }
-
-  static File renderPDF(File svgFile) {
-    def t = new PDFTranscoder()
-    configureFonts(t)
-    String svgURI = svgFile.toURI().toString()
-    File pdfFile = temporaryFile('temporary', '.pdf')
-    try {
-      t.transcode(
-        new TranscoderInput(svgURI),
-        new TranscoderOutput(
-          new FileOutputStream(pdfFile)
-        )
-      )
-    } catch (TranscoderException e) {
-      // Let's be very verbose about error logging, since it's very hard to debug FOP exceptions.
-      log.debug(e)
-      log.debug(e?.exception)
-      log.debug(e?.exception?.cause)
-      throw e
-    }
-    pdfFile
-  }
-
-  private static void configureFonts(SVGAbstractTranscoder t) {
-    DefaultConfigurationBuilder cfgBuilder = new DefaultConfigurationBuilder()
-    Configuration cfg = cfgBuilder.build(rendererConfiguration)
-    ContainerUtil.configure(t, cfg)
-    t.addTranscodingHint(KEY_PIXEL_UNIT_TO_MILLIMETER, new Float((float) (25.4 / DEFAULT_DPI)))
-    t.addTranscodingHint(KEY_XML_PARSER_VALIDATING, FALSE)
-    t.addTranscodingHint(KEY_STROKE_TEXT, FALSE)
-    t.addTranscodingHint(KEY_AUTO_FONTS, false)
-  }
-
-  private static InputStream getRendererConfiguration() {
-    File configFile = new File(insideLambda ? "/var/task/fonts/pdf-renderer-cfg.xml" : "fonts/pdf-renderer-cfg.xml")
-    if (configFile.exists()) {
-      println "DEBUG: Using PDF renderer configuration: ${configFile.absolutePath}"
-      if (insideLambda) {
-        System.setProperty('user.home', System.getProperty('java.io.tmpdir'))
-        def xml = new XmlParser().parse(configFile)
-        xml.fonts.font.each {
-          it.@'embed-url' = '/var/task/' + it.@'embed-url'
-        }
-        return new ByteArrayInputStream(XmlUtil.serialize(xml).getBytes(UTF_8))
-      } else {
-        return configFile.newInputStream()
-      }
-    } else {
-      throw new RuntimeException('PDF renderer configuration is not found!')
-    }
   }
 
 }
